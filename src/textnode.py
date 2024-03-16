2f9f6e79c07e4a07ee5e1d5857310ae90463f632
JJHuus <jajuhhuu@gmail.com>
2024-03-16 11:18:45 +0200

    fixed the __repr__ to also check type and url

--- src/textnode.py
+++ src/textnode.py
@@ -3,16 +3,18 @@ class TextNode:
     text_type = ""
     url = None
 
-    def __init__(self, text1, type1, url1):
+    def __init__(self, text1 = "This is a text node", type1 = "bold", url1="https://www.boot.dev"):
         self.text = text1
         self.text_type = type1
         self.url = url1
         return
     
     def __eq__(self, other):
-        return (self.text == other.text) and ...
-        (self.text_type == other.text_type) and ...
-        (self.url == other.url)
+        return (
+            (self.text == other.text) and 
+            (self.text_type == other.text_type) and 
+            (self.url == other.url)
+            )
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
